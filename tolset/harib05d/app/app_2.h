1b30832e30ab91e5f5f6345a1fac57b3730fc995
Derek2011cc <Derek_13@qq.com>
2022-03-20 20:30:07 +0800

    操作系统

--- tolset/harib05d/app/app_2.h
+++
@@ -1,5 +0,0 @@
-//
-while (1){
-boxfill8(binfo->vram,binfo->scrnx,COL8_008484, 0,0,binfo->scrnx- 1, binfo->scrny - 1);
-putfonts8_asc(binfo->vram, binfo->scrnx, 131, 10, COL8_000000, "whz OS");
-}
--- tolset/harib05d/app/off.h
+++
@@ -1,5 +0,0 @@
-// 关机用
-boxfill8(binfo->vram,binfo->scrnx,COL8_008484, 0,0,binfo->scrnx- 1, binfo->scrny - 1);
-putfonts8_asc(binfo->vram, binfo->scrnx, 85, 60, COL8_000000, "Shutdown in progress");
-for (i=0;i<250000000;i++);
-break;
